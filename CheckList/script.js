60af0a5dd2681d8c2c0547e131b69d3703c2d605
Maas,Midas M <midas.maas@student.fontys.nl>
2020-11-12 10:37:17 +0100

    Functie: welke taken er niet zijn voltooid. Bug fix: vorige/volgende-gebruikerknoppen geven nu geen error messages en stoppen met tellen.

--- CheckList/script.js
+++ CheckList/script.js
@@ -1,4 +1,4 @@
-document.querySelector(".submit").addEventListener("click", checkboxCheck);
+
 
 /*var checked = document.querySelectorAll("input[type='checkbox']");
 
@@ -37,6 +37,91 @@ function checkboxCheck() {
 
 */
 
+document.querySelector(".submit").addEventListener("click", checkboxNames);
+
+document.querySelector(".volgende-gebruiker").addEventListener("click", volgendeGebruiker);
+
+document.querySelector(".vorige-gebruiker").addEventListener("click", vorigeGebruiker);
+
+const listItem1 = document.getElementById("ListItem1");
+const listItem2 = document.getElementById("ListItem2");
+const listItem3 = document.getElementById("ListItem3");
+const listItem4 = document.getElementById("ListItem4");
+
+const gebruiker= document.getElementById("gebruiker");
+
+const elementsList = document.querySelectorAll("#ListItem1, #ListItem2, #ListItem3, #ListItem4");
+const elementsArray = [...elementsList];
+
+let gebruikerChecklists = [{
+    gebruiker: "Kok",
+    listItem1: "Gas uit",
+    listItem2: "Afzuigkap uit",
+    listItem3: "Afzuigkap schoonmaken",
+    listItem4: "Vloer boenen",
+},
+    {
+        gebruiker: "Afwasser",
+        listItem1: "Spoelmachine uit",
+        listItem2: "Stroomschakelaar uit",
+        listItem3: "Rookmelder checken",
+        listItem4: "Vloer boenen",
+    },
+    {
+        gebruiker: "Bediende",
+        listItem1: "Gasten de zaak uit",
+        listItem2: "Stopcontacten check",
+        listItem3: "Stofzuigen in hoekjes",
+        listItem4: "Vloer boenen",
+    },
+    {
+        gebruiker: "Barman",
+        listItem1: "Kassa uit",
+        listItem2: "Vluchtroute checken",
+        listItem3: "Sloten checken",
+        listItem4: "Vloer boenen",
+    }
+];
+
+
+let gebruikerNummer= -1;
+let score = 0;
+
+
+function volgendeGebruiker() {
+    if (gebruikerNummer >= 3){
+
+    } else {
+        gebruikerNummer++;
+        renderChecklist()
+    }
+
+}
+
+function vorigeGebruiker() {
+    if (gebruikerNummer <= 0) {
+
+    } else {
+        --gebruikerNummer;
+        renderChecklist();
+    }
+
+
+}
+
+function renderChecklist() {
+
+    let q = gebruikerChecklists[gebruikerNummer];
+
+
+    gebruiker.innerHTML = "<p>" + q.gebruiker + "</p>";
+    listItem1.innerHTML = q.listItem1;
+    listItem2.innerHTML = q.listItem2;
+    listItem3.innerHTML = q.listItem3;
+    listItem4.innerHTML = q.listItem4;
+
+}
+
 
 function checkboxCheck(){
     let inputElems = document.getElementsByTagName("input"),
@@ -45,5 +130,22 @@ function checkboxCheck(){
         if (inputElems[i].type === "checkbox" && inputElems[i].checked === true){
             count++;
             alert(document.querySelectorAll('input[type="checkbox"]:checked').length);
+            alert(document.getElementById())
         }
     }}
+
+    function checkboxNames() {
+        let inputElems = document.getElementsByTagName("input"),
+            count = 0;
+        for (let i = 0; i < inputElems.length; i++) {
+            if (inputElems[i].type === "checkbox" && inputElems[i].checked === false) {
+                count++;
+
+                console.log(elementsArray[i]);
+
+            }
+
+        }
+
+    }
+
