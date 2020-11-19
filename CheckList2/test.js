0be4b3cf18afefce51a407319db50eb36bc76e28
Maas,Midas M <midas.maas@student.fontys.nl>
2020-11-19 14:21:37 +0100

    Checklist ophalen gelukt.

--- CheckList2/test.js
+++ CheckList2/test.js
@@ -1,12 +1,14 @@
 const db = firebase.firestore();
 var docRef = db.collection("functies").doc("barman");
-
-
+const Taak = document.getElementById('geefTaakWeer');
 
 docRef.get().then(function(docs) {
+
     if (docs.exists) {
-        console.log("Document data:", docs.data().taak1);
-        return `<p>${docs.data()}</p>`;
+        //console.log("Document data:", docs.data().taak1);
+        Taak.innerHTML = docs.data().taak2;
+
+
     } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
@@ -15,22 +17,27 @@ docRef.get().then(function(docs) {
     console.log("Error getting document:", error);
 });
 
+let funct, gebr, taak1, taak2, taak3, taak4;
+document.getElementById("insert").addEventListener("click", selectFunction);
+document.getElementById("select").addEventListener("click", selectFunction);
 
+function ready() {
+    funct = document.getElementById('functie');
+    gebr = document.getElementById('gebruiker');
+    taak1 = document.getElementById('taak1');
+    taak2 = document.getElementById('taak2');
+    taak3 = document.getElementById('taak3');
+    taak4 = document.getElementById('taak4');
+}
 
-/*
-vragenRef
-    .onSnapshot(querySnapshot => {
+function selectFunction() {
+    //ready();
+    docRef.get().then(function(snapshot) {
+        document.getElementById('gebruiker').innerHTML = snapshot.data().gebruiker;
+        document.getElementById('taak1').innerHTML = snapshot.data().taak1;
+        document.getElementById('taak2').innerHTML = snapshot.data().taak2;
+        document.getElementById('taak3').innerHTML = snapshot.data().taak3;
+        document.getElementById('taak4').innerHTML = snapshot.data().taak4;
+    })
 
-        const items = querySnapshot.docs.map(doc => {
-
-            return `<p>${doc.data().taak1}</p><p>${doc.data().taak2}</p>`
-
-
-
-        });
-
-        Taak.innerHTML = items.join('');
-        console.log(items);
-    });
-
- */
+}
