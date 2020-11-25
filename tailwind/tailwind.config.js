82c413188627f054ee090e4621b4dc945b4a8921
Kros,Yanick Y <y.kros@student.fontys.nl>
2020-11-25 12:31:20 +0100

    Commit - Login, Home, TailwindCss

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'color-1': '#4CDFFF',
+        'color-2': '#00687F',
+        'color-3': '#00D1FF',
+        'color-4': '#266F7F',
+        'color-5': '#00A7CC',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+
+  
+}
